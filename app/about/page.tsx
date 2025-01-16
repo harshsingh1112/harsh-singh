974dd381f3bb8355ead204ba3b8dd4fc5ac931c6
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 03:36:26 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,6 @@ import path from 'path';
 import { Fragment, Suspense } from 'react';
 import { readMDXFile } from '../blog/utils';
 import { CustomMDX } from '../components/mdx';
-import TopTracks from '../components/spotify/top-tracks';
 import GithubContributions from './github-contributions/github-contributions';
 import Occupation from './occupation';
 
