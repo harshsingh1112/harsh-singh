48030dde1a7643f80537c8478c4fa433253f0ebb
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 01:00:56 +0530

    Update hero.tsx

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -25,18 +25,17 @@ export default function Hero() {
   return (
     <div>
       <h1 className="sr-only">
-        Hello I&apos;m Dale Larroder, I&apos;m a software developer, and I love building things for
-        the web.
+        Hello I&apos;m Harsh Singh, I&apos;m a data scientist, and I love uncovering insights and solving problems through data.
       </h1>
       <div className="relative z-10 flex h-[calc(100vh-81px)] items-center md:h-[calc(100vh-116px)]">
         <div className="mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">
           <div className="-mt-36">
             <div ref={ref} className="flex cursor-default flex-col space-y-2">
               <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl xl:text-9xl">
-                Dale Larroder
+                Harsh Singh
               </h1>
               <h2 className="text-3xl font-medium opacity-80 sm:text-6xl md:text-6xl xl:text-7xl">
-                I build things for the web.
+                Working towards being a Data Scientist.
               </h2>
               <Link
                 href="/about"
