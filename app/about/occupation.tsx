f74c04c2b4896082d3197d74e9099266e0190ab6
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 00:43:43 +0530

    Update occupation.tsx

--- app/about/occupation.tsx
+++ app/about/occupation.tsx
@@ -4,15 +4,15 @@ export default function Occupation() {
   return (
     <div className="mb-8 flex flex-col-reverse items-center justify-between sm:flex-row sm:items-center">
       <div className="text-left">
-        <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Dale Larroder</h1>
-        <h2 className="text-sm font-normal md:text-base">Software Developer</h2>
+        <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Harsh Singh</h1>
+        <h2 className="text-sm font-normal md:text-base">Data Scientist/Web Developer</h2>
       </div>
       <div>
         <Image
-          alt="Dale Larroder"
+          alt="Harsh Singh"
           height={130}
           width={130}
-          src="/static/images/avatar.webp"
+          src="/static/images/avatar.jpeg"
           className="rounded-full object-scale-down grayscale"
         />
       </div>
