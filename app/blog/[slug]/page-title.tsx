503a6ba6e3b207c286d8d8bbad82a6afe80b7535
Harsh Singh <harshsingh34418@gmail.com>
2025-01-18 02:00:23 +0530

    Update page-title.tsx

--- app/blog/[slug]/page-title.tsx
+++ app/blog/[slug]/page-title.tsx
@@ -1,3 +1,7 @@
+import { ReactNode } from 'react';
+interface Props {
+  children: ReactNode;
+}
 export default function PageTitle({ title }: { title: string }) {
   return <h1 className="text-3xl font-bold">{title}</h1>;
-}
+};
