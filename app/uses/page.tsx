6ff183c3caf24a82ade450fb1b63a6bf9c8b9eec
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 16:53:18 +0530

    Update page.tsx

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -8,8 +8,8 @@ const contentPath = path.join(process.cwd(), 'app', 'uses', 'content.mdx');
 const { content } = readMDXFile(contentPath);
 
 export const metadata = {
-  title: 'Uses',
-  description: 'What I use',
+  title: 'Experience',
+  description: 'My Experience',
 };
 
 export default function Page() {
