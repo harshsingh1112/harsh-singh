1247d30012c82834b57e3b67ba28afd40f182d78
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 17:20:44 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import { readMDXFile } from '../blog/utils';
 import { CustomMDX } from '../components/mdx';
 import GithubContributions from './github-contributions/github-contributions';
 import Occupation from './occupation';
+import Resume from './app/about/resume.tsx'
 
 const contentPath = path.join(process.cwd(), 'app', 'about', 'content.mdx');
 const { content } = readMDXFile(contentPath);
@@ -19,16 +20,7 @@ export default function Page() {
       <Occupation />
       <CustomMDX source={content} />
       <GithubContributions />
-       {/* Resume Download Button */}
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <a
-          href="https://drive.google.com/file/d/1m_M-OAivyVGhphPNLTImkP2oFi6F75fm/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button"
-        >
-          Download Resume
-        </a>
+      <Resume />
       </div>
     </Fragment>
   );
