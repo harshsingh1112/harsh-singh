a6f0cbfe1511a33d23b659efec0a4bb398b5fb94
Harsh Singh <harshsingh34418@gmail.com>
2025-01-18 01:18:36 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,8 +3,8 @@ import { BlogPosts } from '../components/posts';
 import { getBlogPosts } from './utils';
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Certificates',
+  description: 'Check out my certifications.',
 };
 
 export default function Page() {
@@ -14,7 +14,7 @@ export default function Page() {
     <MainLayout>
       <section>
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-          Blog
+          Certificates
         </h1>
         <BlogPosts posts={posts} />
       </section>
