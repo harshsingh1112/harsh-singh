2ccd2145acff1b0c6fb0bea5ebe8be071da54f5e
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 17:19:20 +0530

    Create resume.tsx

---
+++ app/about/resume.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ResumeLink: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <strong>
+            <a
+              href="https://drive.google.com/file/d/1m_M-OAivyVGhphPNLTImkP2oFi6F75fm/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+          </strong>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ResumeLink;
