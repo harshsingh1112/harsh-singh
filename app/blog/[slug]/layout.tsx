16ee27c5daf0a97cb26222964c23c8b68f2a5967
Harsh Singh <harshsingh34418@gmail.com>
2025-01-18 02:08:15 +0530

    Update layout.tsx

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto p-4">
       <header className="mb-6">
-        <h1 className="text-2xl">Certification Details</h1>
+        <h1 className="text-2xl">Certifications</h1>
       </header>
       {children}
     </div>
