8b26025ad7a0adab90ee5d407c42956396a5a85d
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 17:30:15 +0530

    Update resume.tsx

--- app/about/resume.tsx
+++ app/about/resume.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
+import styles from './ResumeLink.module.css';  // Import the CSS module
 
 const ResumeLink: React.FC = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <strong>
-            <a
-              href="https://drive.google.com/file/d/1m_M-OAivyVGhphPNLTImkP2oFi6F75fm/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Resume
-            </a>
-          </strong>
-        </li>
-      </ul>
+    <div className={styles.resumeLinkContainer}>
+      <a
+        href="https://drive.google.com/file/d/1m_M-OAivyVGhphPNLTImkP2oFi6F75fm/view?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.resumeLink}  // Apply the class from the CSS module
+      >
+        Resume
+      </a>
     </div>
   );
 };
