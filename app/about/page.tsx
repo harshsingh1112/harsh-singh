b2bb95cf279ba9e815bd1238bfd67da9ba9b6c94
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 17:42:35 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
     <Fragment>
       <Occupation />
       <CustomMDX source={content} />
-      <GithubContributions />
       <Resume />
+      <GithubContributions />
     </Fragment>
   );
 }
