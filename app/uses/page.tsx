8c6514a5f7b9c4376e787eb22da935af492e51a9
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 21:46:29 +0530

    Update page.tsx

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -7,21 +7,12 @@ import UsesTitle from './uses-title';
 const contentPath = path.join(process.cwd(), 'app', 'uses', 'content.mdx');
 const { content } = readMDXFile(contentPath);
 
-// Debugging log
-if (!content) {
-  console.error('MDX content is null or undefined');
-}
-
 export const metadata = {
   title: 'Experience',
   description: 'My Experience',
 };
 
 export default function Page() {
-  if (!content) {
-    return <div>Error: Content could not be loaded</div>;
-  }
-
   return (
     <Fragment>
       <UsesTitle />
