6be7266b2769c6253a3c556385d3e35b2a91385b
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 17:10:21 +0530

    Update constants.ts

--- app/components/layouts/constants.ts
+++ app/components/layouts/constants.ts
@@ -1,6 +1,7 @@
 export const navigationLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/uses', title: 'Experience' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Certificates' },
+  
 ];
