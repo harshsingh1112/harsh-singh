59b84c56b5811011a5ac140342d47b4810dadcfe
Harsh Singh <harshsingh34418@gmail.com>
2025-01-17 13:54:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="76x76" href="public/static/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="public/static/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="public/static/favicons/favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
