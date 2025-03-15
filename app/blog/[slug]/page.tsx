cd549e3e243649db178f9272b3219242d187b0e7
Harsh Singh <harshsingh34418@gmail.com>
2025-03-15 13:58:33 +0530

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,24 +1,11 @@
-export async function generateMetadata(props: { params: Promise<{ slug: string }> }) {
-  const params = await props.params;
-  
-
-  return {
-    title: "Default Title",  
-    description: "Default description",  
-  };
-}
-
 export default async function Blog(props: { params: Promise<{ slug: string }> }) {
   const params = await props.params;
 
-
   return (
     <section>
-      {/* If `PageTitle` is required, re-add the import and use it here */}
       <h1>Blog Title</h1>
 
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        {/* If `formatDate` is required, re-add the import and use it here */}
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           Published Date
         </p>
