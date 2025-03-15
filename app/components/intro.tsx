46dfe914f8b7273203ef8489a2144bc928b4ab0d
Harsh Singh <harshsingh34418@gmail.com>
2025-03-15 12:24:17 +0530

    Update intro.tsx

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -46,7 +46,7 @@ export default function Intro() {
       <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center px-10 py-24 text-4xl font-semibold tracking-tight md:py-28 md:text-6xl lg:px-20 lg:py-3 lg:text-7xl">
         <div className="leading-[1.15]">
           <div className="introText" style={{ opacity: opacityForBlock(progress, 0) }}>
-            I love coding.
+            Passionate about leveraging code to analyze data and drive business solutions.
           </div>
           <span
             className="introText inline-block after:content-['_']"
@@ -58,7 +58,7 @@ export default function Intro() {
             className="introText inline-block"
             style={{ opacity: opacityForBlock(progress, 2) }}
           >
-            I&apos;m passionate about deriving insights from data, building models, and solving complex problems.
+            I&apos;m passionate about deriving insights from data, building models, and solving business problems.
           </span>
         </div>
       </div>
