78e5742278eb4f6c22eaf7a287979fa121a0a733
Harsh Singh <harshsingh34418@gmail.com>
2025-07-08 16:15:23 +0400

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,10 @@
 export default async function Blog(props: { params: Promise<{ slug: string }> }) {
   const params = await props.params;
+  const { slug } = params;
 
   return (
     <section>
-      <h1>Blog Title</h1>
+      <h1>{slug}</h1> {/* Displays the slug as blog title for now */}
 
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
