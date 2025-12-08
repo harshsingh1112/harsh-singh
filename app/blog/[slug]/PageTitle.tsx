305468e2ac8696bd49ea9a99024a36c52ece3e53
Harsh Singh <harshsingh34418@gmail.com>
2025-12-08 22:54:35 +0400

    Delete app/blog directory

--- app/blog/[slug]/PageTitle.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({ children }: { children: React.ReactNode }) {
-  return (
-    <h1 className="text-3xl font-bold tracking-tight text-black dark:text-white">
-      {children}
-    </h1>
-  );
-}
