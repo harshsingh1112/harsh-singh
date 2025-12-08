38f76889aecb4b61b0df9be4c40c542b00e2db31
Harsh Singh <harshsingh34418@gmail.com>
2025-12-08 23:08:10 +0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,9 @@
+// app/sitemap.ts
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://harsh-singh.vercel.app";
+const baseUrl = "https://harsh-singh.vercel.app";
 
+export default function sitemap(): MetadataRoute.Sitemap {
   const routes = ["", "projects", "about", "experience"].map((route) => ({
     url: `${baseUrl}/${route}`,
     lastModified: new Date(),
