af26a151e7eab36e61a735c6c2a06f4733a50980
Harsh Singh <harshsingh34418@gmail.com>
2025-12-09 23:43:00 +0400

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx'],
-  transpilePackages: ['next-mdx-remote'],
-};
-
-export default nextConfig;
